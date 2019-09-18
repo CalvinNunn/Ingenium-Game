43b8ad64b2d82a014ac985421623b214f2e42f6d
Christian <christian.ceccarelli@ontariotechu.net>
2019-09-18 19:56:02 -0400

    Working on Raycasting reticle

---
+++ IngeniumGame/Assets/Scripts/ReticleControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReticleControl : MonoBehaviour
+{
+    public Camera c;
+    public Ray ray;
+    public GameObject reticle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+       
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        reticle.transform.position = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y, transform.position.z);
+        Debug.Log(ray);
+    }
+}
