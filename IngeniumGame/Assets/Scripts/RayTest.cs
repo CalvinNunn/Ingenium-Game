f5b0176e8d2c9c9b58d6dd68750acd3ffaa4cc72
cchecker4 <christian.ceccarelli@ontariotechu.net>
2019-09-18 23:18:49 -0400

    finished reticle following mouse position

---
+++ IngeniumGame/Assets/Scripts/RayTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider != null)
+                {
+                    hit.collider.enabled = false;
+                }
+            }
+
+        }
+    }
+}
+
--- IngeniumGame/Assets/Scripts/ReticleControl.cs
+++ IngeniumGame/Assets/Scripts/ReticleControl.cs
@@ -4,13 +4,10 @@ using UnityEngine;
 
 public class ReticleControl : MonoBehaviour
 {
-    public Camera c;
-    public Ray ray;
-    public GameObject reticle;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.visible = false;
        
         
     }
@@ -18,7 +15,11 @@ public class ReticleControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        reticle.transform.position = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y, transform.position.z);
-        Debug.Log(ray);
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.visible = true;
+        }
+
+        transform.position = Input.mousePosition;
     }
 }
